8264060d933258fe0a8d612b83392568d45cda9c
Richard Davis <rdavis.bacs@gmail.com>
2015-02-02 12:37:34 -0500

    only run factories spec in test environment

--- backend/spec/factories/factories_spec.rb
+++ backend/spec/factories/factories_spec.rb
@@ -1,9 +1,11 @@
-require "rails_helper"
+if Rails.env.test?
+  require "rails_helper"
 
-RSpec.describe "Factories" do
-  FactoryGirl.factories.map(&:name).each do |factory|
-    it "has a valid #{factory} factory" do
-      expect(FactoryGirl.build(factory)).to be_valid
+  RSpec.describe "Factories" do
+    FactoryGirl.factories.map(&:name).each do |factory|
+      it "has a valid #{factory} factory" do
+        expect(FactoryGirl.build(factory)).to be_valid
+      end
     end
   end
 end
