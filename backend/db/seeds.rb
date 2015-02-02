218c832a96a9e0721804e7baaae82fccd53ed6c5
Richard Davis <rdavis.bacs@gmail.com>
2015-02-02 14:18:06 -0500

    seed projects

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+projects = [
+  { name: "LaunchVotes GitHub Team Integration",
+    description: "Make it so that team members can only vote, nominate, and view awards for their teammates." },
+
+  { name: "Noise Toaster",
+    description: "A DIY analog synth kit." },
+
+  { name: "Multiple Choice App",
+    description: "An application that allows instructors to easily create and administer multiple-choice quizzes." }
+]
+
+projects.each do |project_params|
+  project = Project.find_or_create_by!(name: project_params[:name])
+  project.update_attributes!(project_params)
+end
